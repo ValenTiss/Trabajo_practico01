c4c7a1170ba72acf4f5880dcc3c1d1ba317c851c
ValenTiss <100385088+ValenTiss@users.noreply.github.com>
2022-05-19 23:26:13 -0400

    Update ordenamiento_media.py

--- ordenamiento_media.py
+++ ordenamiento_media.py
@@ -18,7 +18,7 @@ class Ordenamiento():
     numero_medio = int(len(copia_lista)/2)
     return copia_lista[numero_medio]
 
-
+  def quick_sort(self,lista):
     lista_fin = lista.copy()
 
     if len(lista)>1:
@@ -60,81 +60,81 @@ class Ordenamiento_2():
     return copia_lista[numero_medio]
 
   def quick_sort(self,lista):
-  """
-  Ordena la lista usando el lgoritmo de ordenamiento quick sort
-  Entradas: lista a ordenar
-  Return: lista ordenada
-  """
-  if len(lista)>1:
-    return self.quick_sort(menores(lista[0], lista[1:],0,[])) + [lista[0]] + quick_sort(mayores(lista[0],lista[1:],0,[]))
-  else:
-    return lista
-
-def mayores(self, valor,lista, contador,lista_mayores):
-  """
-  Crea una lista con los valores mayores a un valor dado
-  parametro 1: Número con el cual se comparan los números de la lista
-  parametro 2: Lista de la cual se desea comparar cada número con valor
-  parametro 3: Cuenta la cantidad de iteraciones, sirve para determinar el indice del valor a comparar
-  parametro 4: Lista que almacenará los valores de la lista que son mayores que el el número que se recibe en la primer entrada de la función
-  Return: Lista con los valores de la lista mayores a valor
-  """
-  #Si la lista está vacía o tiene un solo elemento
-  if len(lista)>1:
-    #Si no se ha evaluado el último elemento de la lista
-    if contador!=len(lista):
-      #Si el número de la lista es mayor o igual que valor
-      if valor <= lista[contador]:
-        #Agrega el número mayor al final de la lista que contiene los números mayores
-        lista_mayores.append(lista[contador])
-        contador+=1
-        #Llamado recursivo
-        return self.mayores(valor, lista, contador, lista_mayores)
-      #El número de la lista es menor a valor
-      else:
-        contador+=1
-        return self.mayores(valor,lista, contador, lista_mayores)
-    #Se recorrió toda la lista 
+    """
+    Ordena la lista usando el lgoritmo de ordenamiento quick sort
+    Entradas: lista a ordenar
+    Return: lista ordenada
+    """
+    if len(lista)>1:
+      return self.quick_sort(self.menores(lista[0], lista[1:],0,[])) + [lista[0]] + self.quick_sort(self.mayores(lista[0],lista[1:],0,[]))
     else:
-      return lista_mayores
-  #Si el único valor de la lista es mayor a valor
-  elif lista[0]>=valor:
-    return lista
-  #La lista está vacía
-  else:
-      return[]
+      return lista
 
-def menores(self, valor,lista, contador,lista_menores):
-  """
-  Crea una lista con los valores mayores a un valor dado
-  parametro 1: Número con el cual se comparan los números de la lista
-  parametro 2: Lista de la cual se desea comparar cada número con valor
-  parametro 3: Cuenta la cantidad de iteraciones, sirve para determinar el indice del valor a comparar
-  parametro 4: Lista que almacenará los valores de la lista que son mayores que el el número que se recibe en la primer entrada de la función
-  Return: Lista con los valores de la lista mayores a valor
-  """
-  #Si la lista está vacía o tiene un solo elemento
-  if len(lista)>1 :
-    #Si no se ha evaluado el último elemento de la lista
-    if len(lista)>1 and contador!=len(lista):
-      #Si el número de la lista es menor que valor
-      if valor >lista[contador]:
-        #Agrega el número menor al final de la lista que contiene los números menores
-        lista_menores.append(lista[contador])
-        contador+=1
-        #Llamado recursivo
-        return self.menores(valor,lista, contador, lista_menores)
-      #El número de la lista es menor a valor
+  def mayores(self, valor,lista, contador,lista_mayores):
+    """
+    Crea una lista con los valores mayores a un valor dado
+    parametro 1: Número con el cual se comparan los números de la lista
+    parametro 2: Lista de la cual se desea comparar cada número con valor
+    parametro 3: Cuenta la cantidad de iteraciones, sirve para determinar el indice del valor a comparar
+    parametro 4: Lista que almacenará los valores de la lista que son mayores que el el número que se recibe en la primer entrada de la función
+    Return: Lista con los valores de la lista mayores a valor
+    """
+    #Si la lista está vacía o tiene un solo elemento
+    if len(lista)>1:
+      #Si no se ha evaluado el último elemento de la lista
+      if contador!=len(lista):
+        #Si el número de la lista es mayor o igual que valor
+        if valor <= lista[contador]:
+          #Agrega el número mayor al final de la lista que contiene los números mayores
+          lista_mayores.append(lista[contador])
+          contador+=1
+          #Llamado recursivo
+          return self.mayores(valor, lista, contador, lista_mayores)
+        #El número de la lista es menor a valor
+        else:
+          contador+=1
+          return self.mayores(valor,lista, contador, lista_mayores)
+      #Se recorrió toda la lista 
       else:
-        contador+=1
-        return self.menores(valor,lista, contador, lista_menores)
-    #Se recorrió toda la lista
+        return lista_mayores
+    #Si el único valor de la lista es mayor a valor
+    elif lista[0]>=valor:
+      return lista
+    #La lista está vacía
     else:
-      return lista_menores
-  #Si el único valor de la lista es mayor a valor
-  elif lista[0]<valor:
-    return lista
-  #La lista está vacía
-  else:
-      return[]
+        return[]
+
+  def menores(self, valor,lista, contador,lista_menores):
+    """
+    Crea una lista con los valores mayores a un valor dado
+    parametro 1: Número con el cual se comparan los números de la lista
+    parametro 2: Lista de la cual se desea comparar cada número con valor
+    parametro 3: Cuenta la cantidad de iteraciones, sirve para determinar el indice del valor a comparar
+    parametro 4: Lista que almacenará los valores de la lista que son mayores que el el número que se recibe en la primer entrada de la función
+    Return: Lista con los valores de la lista mayores a valor
+    """
+    #Si la lista está vacía o tiene un solo elemento
+    if len(lista)>1 :
+      #Si no se ha evaluado el último elemento de la lista
+      if len(lista)>1 and contador!=len(lista):
+        #Si el número de la lista es menor que valor
+        if valor >lista[contador]:
+          #Agrega el número menor al final de la lista que contiene los números menores
+          lista_menores.append(lista[contador])
+          contador+=1
+          #Llamado recursivo
+          return self.menores(valor,lista, contador, lista_menores)
+        #El número de la lista es menor a valor
+        else:
+          contador+=1
+          return self.menores(valor,lista, contador, lista_menores)
+      #Se recorrió toda la lista
+      else:
+        return lista_menores
+    #Si el único valor de la lista es mayor a valor
+    elif lista[0]<valor:
+      return lista
+    #La lista está vacía
+    else:
+        return[]
 

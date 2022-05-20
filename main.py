ec08e47e5f5f87fcba6af8ec78ecf2c0a7f2bf3a
FedeAlfaro <71413273+FedeAlfaro@users.noreply.github.com>
2022-05-20 12:55:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -17,9 +17,10 @@ cm = Calculo_Media(matriz)
 #Tamaño de la ventana
 dimension_ventana = int(input("Ingrese el tamaño de la ventana: "))
 #Opción de algoritmo de ordenamiento que se quiera usar
-selector = int(input("Ingrese el algoritmo de ordenamiento que quiere usar (1) o (2)"))
+selector = input("Ingrese el algoritmo de ordenamiento que quiere usar (1) o (2)")
 #Si los datos introducidos por el usuario son los esperados
-if isinstance(dimension_ventana,int) and dimension_ventana % 2 == 1 and isinstance(selector,int) and (1 == selector or selector == 2):
+if isinstance(dimension_ventana,int) and dimension_ventana % 2 == 1 and ("1" == selector or selector == "2"):
+  selector = int(selector)
   #Calcula 2 veces el filtro de medianas
   resultado= cm.calcular_matriz_de_medias(cm.calcular_matriz_de_medias(matriz,0,dimension_ventana,selector),0,dimension_ventana,selector)
   #Calcula el filtro de medianas nuevamente y muestra la imagen generada
